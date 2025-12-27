eda9f5b1b7fd736ba6883aae61da81d81581ea57
Victor Hugo <victorufgcc@hotmail.com>
2025-12-26 21:03:10 -0300

    feat: configure Laravel Passport for OAuth2 authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Passport\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
