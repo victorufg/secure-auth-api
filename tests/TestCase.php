ba751c804fc4c91a998787be4e7d05059f04b301
Victor Hugo <victorufgcc@hotmail.com>
2025-12-26 22:01:08 -0300

    fix: create Passport client programmatically in TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,4 +8,17 @@ use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 abstract class TestCase extends BaseTestCase
 {
     use LazilyRefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Create Personal Access Client if it doesn't exist
+        if (!\Laravel\Passport\Client::where('personal_access_client', true)->exists()) {
+            $this->artisan('passport:client', [
+                '--personal' => true,
+                '--name' => 'Test Personal Access Client',
+            ]);
+        }
+    }
 }
