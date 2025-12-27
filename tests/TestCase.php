40c404aced671bda662ec6f41fb724bfc57a33ef
Victor Hugo <victorufgcc@hotmail.com>
2025-12-26 22:03:16 -0300

    fix: create Passport client via DB instead of artisan command

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,9 +15,18 @@ abstract class TestCase extends BaseTestCase
         
         // Create Personal Access Client if it doesn't exist
         if (!\Laravel\Passport\Client::where('personal_access_client', true)->exists()) {
-            $this->artisan('passport:client', [
-                '--personal' => true,
-                '--name' => 'Test Personal Access Client',
+            $client = \Laravel\Passport\Client::create([
+                'name' => 'Test Personal Access Client',
+                'secret' => null,
+                'provider' => 'users',
+                'redirect' => 'http://localhost',
+                'personal_access_client' => true,
+                'password_client' => false,
+                'revoked' => false,
+            ]);
+            
+            \Laravel\Passport\PersonalAccessClient::create([
+                'client_id' => $client->id,
             ]);
         }
     }
